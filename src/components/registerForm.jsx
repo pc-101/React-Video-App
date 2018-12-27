e845411e672b75e5bd1ebde1ea80988fbf990eb6
Peter <longkchea@gmail.com>
2018-12-27 02:01:50 -0800

    Added in a register form so users can register for service

---
+++ src/components/registerForm.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Form from "./common/form";
+import Joi from "joi-browser";
+
+class RegisterForm extends Form {
+  state = {
+    data: {
+      username: "",
+      password: "",
+      name: ""
+    },
+    errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .email()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .min(5)
+      .label("Password"),
+    name: Joi.string()
+      .required()
+      .label("Name")
+  };
+
+  doSubmit = () => {
+    // Send to server
+    console.log("Submitting new user data and sending to server!");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name", "name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
