110ea8c69de892b9d40a4557ce3f07a9722fdebd
Peter <longkchea@gmail.com>
2019-01-03 00:15:56 -0800

    Introduced route-level code splitting and added a loading message for users to see while components are being loaded in

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React, { Component } from "react";
+import React, { Component, Suspense, lazy } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar";
-import Movies from "./components/movies";
-import Customers from "./components/customers";
-import Rentals from "./components/rentals";
-import NotFound from "./components/notFound";
-import MovieForm from "./components/movieForm";
-import LoginForm from "./components/loginForm";
-import RegisterForm from "./components/registerForm";
 import "./App.css";
 
+const Movies = lazy(() => import("./components/movies"));
+const Customers = lazy(() => import("./components/customers"));
+const Rentals = lazy(() => import("./components/rentals"));
+const NotFound = lazy(() => import("./components/notFound"));
+const MovieForm = lazy(() => import("./components/movieForm"));
+const LoginForm = lazy(() => import("./components/loginForm"));
+const RegisterForm = lazy(() => import("./components/registerForm"));
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <NavBar />
         <main className="container">
-          <Switch>
-            <Route path="/register" component={RegisterForm} />
-            <Route path="/login" component={LoginForm} />
-            <Route path="/movies/:id" component={MovieForm} />
-            <Route path="/movies" component={Movies} />
-            <Route path="/customers" component={Customers} />
-            <Route path="/rentals" component={Rentals} />
-            <Route path="/not-found" component={NotFound} />
-            <Redirect exact from="/" to="/movies" />
-            <Redirect to="/not-found" />
-          </Switch>
-          {/* <Movies /> */}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/register" component={RegisterForm} />
+              <Route path="/login" component={LoginForm} />
+              <Route path="/movies/:id" component={MovieForm} />
+              <Route path="/movies" component={Movies} />
+              <Route path="/customers" component={Customers} />
+              <Route path="/rentals" component={Rentals} />
+              <Route path="/not-found" component={NotFound} />
+              <Redirect exact from="/" to="/movies" />
+              <Redirect to="/not-found" />
+            </Switch>
+            {/* <Movies /> */}
+          </Suspense>
         </main>
       </React.Fragment>
     );
